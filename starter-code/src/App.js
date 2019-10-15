675a3c81db7c723d05cac21bf977d48c63f5074c
Julieta <jmartinezrey@hotmail.com>
2019-10-15 11:23:59 +0200

    Done

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+class Navbar extends Component {
+    render(){
+      return(
+      <div className="navbar-container">
+        <Navbar />
+      </div>
+      )
+    }
+  }
+  
+  export default Navbar;
---
+++ starter-code/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Navbar(props){
+    return(
+        <h1>Hola</h1>
+    )
+}
+
+export default Navbar
